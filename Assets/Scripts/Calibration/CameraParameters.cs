37ac33613e20cb67194045cb96a561bc14a3b8c9
Hanseul Jun <hanseuljun@gmail.com>
2016-01-29 18:08:33 +0900

    add camera parameter

---
+++ Assets/Scripts/Calibration/CameraParameters.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraParameters {
+
+	public float ScaleX { get; private set; }
+	public float ScaleY { get; private set; }
+	public float OffsetX { get; private set; }
+	public float OffsetY { get; private set; }
+
+	private CameraParameters() {
+	}
+
+	public static CameraParameters CreateMetaDepth() {
+		CameraParameters cp = new CameraParameters();
+		cp.ScaleX = 1.0f;
+		cp.ScaleY = 1.0f;
+		cp.OffsetX = 0.0f;
+		cp.OffsetY = 0.0f;
+
+		return cp;
+	}
+}
